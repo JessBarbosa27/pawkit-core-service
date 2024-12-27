7732405d7bc16fb81dc128401a155bafb2d4c836
JessBarbosa27 <barbosajess27@gmail.com>
2024-12-27 20:55:40 +0000

    initial commit

---
+++ src/main/java/com/jessbarbosa/apps/pawkit_core_service/PawkitCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jessbarbosa.apps.pawkit_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawkitCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawkitCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessbarbosa/apps/pawkit_core_service/PawkitCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessbarbosa.apps.pawkit_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawkitCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
